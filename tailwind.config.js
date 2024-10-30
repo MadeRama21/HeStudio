8a8c3dfb48a167395490be4d11627f1b4e314860
MadeRama <dwijaaa21@gmail.com>
2024-10-30 21:39:34 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {
+      fontFamily: {
+        Primary: ["DM Serif Display"],
+        Secondary: ["Montserrat"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#292f36",
+          hover: "343e4a",
+        },
+        secondary: "#4d5053",
+        accent: {
+          DEFAULT: "#cda274",
+          secondary: "#f4f0ec",
+          hover: "#b88c5d",
+        },
+        putih: "#d9c8ae",
+      },
+    },
+  },
+  plugins: [],
+};
